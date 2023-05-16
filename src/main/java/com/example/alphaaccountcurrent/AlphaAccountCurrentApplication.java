a4a0312292a223d958ef3bc33406a6f19b957a3c
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-16 10:01:17 +0000

    alpha account current update

---
+++ src/main/java/com/example/alphaaccountcurrent/AlphaAccountCurrentApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphaaccountcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAccountCurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaAccountCurrentApplication.class, args);
+    }
+
+}
